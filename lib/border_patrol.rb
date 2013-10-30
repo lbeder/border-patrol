b504d8bd01836d68e2373191be67ec29c02b8f80
Leonid Beder <leonid@jammer.fm>
2013-10-30 18:14:10 +0200

    Abort server's start.

--- lib/border_patrol.rb
+++ lib/border_patrol.rb
@@ -7,6 +7,23 @@ module BorderPatrol
     self.configuration ||= Configuration.new
 
     yield configuration
+
+    abort_if_pending
+  end
+
+  private
+  def pending_migrations
+    ActiveRecord::Migrator.new(:up, ActiveRecord::Migrator.migrations_paths).pending_migrations
+  end
+
+  def abort_if_pending
+    if pending_migrations.any?
+      puts "You have #{pending_migrations.size} pending migrations:"
+      pending_migrations.each do |pending_migration|
+        puts '  %4d %s' % [pending_migration.version, pending_migration.name]
+      end
+      abort %{Run `rake db:migrate` to update your database then try again.}
+    end
   end
 end
 
